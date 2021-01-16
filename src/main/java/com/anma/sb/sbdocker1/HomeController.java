754693ea8553cbb229f8aada076357c7f5ac98e7
Andrii maliuta <quadr988@gmail.com>
2021-01-16 15:20:31 +0200

    Dockerfile

--- src/main/java/com/anma/sb/sbdocker1/HomeController.java
+++ src/main/java/com/anma/sb/sbdocker1/HomeController.java
@@ -5,13 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String getHomePgae(Model model) {
-        model.addAttribute("date", LocalDate.now().toString());
+        List<LocalDate> weekDates = new ArrayList<>();
+        LocalDate now = LocalDate.now();
+        weekDates.add(now);
+        for (int i = 1; i <= 7; i++) {
+            weekDates.add(now.minusDays(i));
+        }
+        model.addAttribute("weekDates", weekDates);
         return "home";
     }
 }
